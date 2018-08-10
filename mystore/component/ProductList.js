d944d3d5ca5f7d31fe4540d07effd1e58b1505e1
Pankaj Singh <singh.pankaj@live.com>
2018-08-10 11:43:37 +0530

    pagination

--- mystore/component/ProductList.js
+++ mystore/component/ProductList.js
@@ -10,19 +10,22 @@ import {
 class ProductList extends Component {
   constructor(props) {
     super(props);
-    this.state = { products: [], isLoading: true };
+    this.state = { products: [], isLoading: true, page: 1, limit: 20 };
   }
 
   async componentDidMount() {
-    let response = await fetch("http://localhost:4000/products");
+    let { page, limit } = this.state;
+    let response = await fetch(
+      `http://localhost:4000/products?_page=${page}&_limit=${limit}`
+    );
     let products = await response.json();
-    this.setState({ products, isLoading: false });
+    this.setState({ products, isLoading: false, page: page + 1 });
   }
 
   _renderItem({ item: product }) {
     return (
       <Text
-        style={{ backgroundColor: "lightgreen", margin: 5 }}
+        style={{ backgroundColor: "lightgreen", marginBottom: 25 }}
         key={product.id}
       >
         {product.title}
@@ -34,6 +37,22 @@ class ProductList extends Component {
     return `${p.id}`;
   }
 
+  async _getMore() {
+    let { page, limit } = this.state;
+    console.log(
+      `http://localhost:4000/products?_page=${page}&_limit=${limit}`
+    );
+    let response = await fetch(
+      `http://localhost:4000/products?_page=${page}&_limit=${limit}`
+    );
+    let products = await response.json();
+    this.setState({
+      products: [...this.state.products, ...products],
+      isLoading: false,
+      page: page + 1
+    });
+  }
+
   render() {
     const { products, isLoading } = this.state;
     return (
@@ -43,6 +62,8 @@ class ProductList extends Component {
           data={products}
           keyExtractor={this._keyExtractor}
           renderItem={this._renderItem}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => this._getMore()}
         />
       </View>
     );
