6ea872677b08ec09b1511eb3fa5a4cf9675555e0
Pankaj Singh <singh.pankaj@live.com>
2018-08-10 10:42:07 +0530

    commit

--- mystore/component/ProductList.js
+++ mystore/component/ProductList.js
@@ -1,27 +1,59 @@
 import React, { Component } from "react";
-import { ScrollView ,Text, ActivityIndicator}  from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  FlatList
+} from "react-native";
 
-class ProductList extends Component{
-    constructor(props) {
-        super(props);
-        this.state = { products: [], isLoading: true};
-    }
-    async componentDidMount() {
-        let response = await fetch("http://localhost:4000/products");
-        let products = await response.json();
-        this.setState({ products, isLoading: false});
-    }
-    render() {
-        const { products, isLoading} = this.state;
-        return(
-            <ScrollView>
-                {isLoading && <ActivityIndicator size="large" color="green"/>}
-                {products.map(p=>(
-                    <Text style={{backgroundColor:"forestgreen",margin:5}} key={p.id}>{p.title}</Text>
-                ))}
-            </ScrollView>
-        );
-    }
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [], isLoading: true };
+  }
+
+  async componentDidMount() {
+    let response = await fetch("http://localhost:4000/products");
+    let products = await response.json();
+    this.setState({ products, isLoading: false });
+  }
+
+  _renderItem({ item: product }) {
+    return (
+      <Text
+        style={{ backgroundColor: "lightgreen", margin: 5 }}
+        key={product.id}
+      >
+        {product.title}
+      </Text>
+    );
+  }
+
+  _keyExtractor(p, i) {
+    return `${p.id}`;
+  }
+
+  render() {
+    const { products, isLoading } = this.state;
+    return (
+      <View>
+        {isLoading && <ActivityIndicator size="large" color="green" />}
+        <FlatList
+          data={products}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
+      </View>
+    );
+  }
 }
+
 export default ProductList;
 
+// <ScrollView>
+// {isLoading && <ActivityIndicator size="large" color="green" />}
+// {products.map(p => (
+
+// ))}
+// </ScrollView>
